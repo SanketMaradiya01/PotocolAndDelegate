ef1a5af471c3fd24bf920012fda4b342e0257d54
SanketMaradiya01 <sanketmaradiya@nimapinfotech.com>
2024-02-12 12:37:11 +0530

    Protocol Changes

---
+++ PotocolAndDelegate/Controllers/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  PotocolAndDelegate
+//
+//  Created by Nimap on 12/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController, DataPass {
+    
+    
+    
+    var FillButton : UIButton!
+    var UserNameLabel : UILabel!
+    var PasswordLabel : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DisplayView()
+    }
+    func DisplayView(){
+        
+        view.backgroundColor = .white
+        
+        FillButton = UIButton(type: .system)
+        FillButton.setTitle("Fill Form", for: .normal)
+        FillButton.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        FillButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        view.addSubview(FillButton)
+        FillButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            FillButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            FillButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            FillButton.widthAnchor.constraint(equalToConstant: 200),
+//            FillButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        PasswordLabel = UILabel()
+        PasswordLabel.translatesAutoresizingMaskIntoConstraints = false
+        PasswordLabel.text = "Password"
+        PasswordLabel.textColor = .black
+        PasswordLabel.textAlignment = .center
+        view.addSubview(PasswordLabel)
+        NSLayoutConstraint.activate([
+            PasswordLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            PasswordLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            PasswordLabel.bottomAnchor.constraint(equalTo: FillButton.topAnchor, constant: -50)
+        ])
+        
+        UserNameLabel = UILabel()
+        UserNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        UserNameLabel.text = "UserName"
+        UserNameLabel.textAlignment = .center
+        UserNameLabel.textColor = .black
+        view.addSubview(UserNameLabel)
+        NSLayoutConstraint.activate([
+            UserNameLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            UserNameLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            UserNameLabel.bottomAnchor.constraint(equalTo: PasswordLabel.topAnchor, constant: -50)
+        ])
+        
+    }
+    @objc func buttonPressed() {
+        var vc = SecondViewController()
+        vc.delegate = self
+        self.navigationController?.pushViewController(vc, animated: true)
+        print("Fill Btn Pressed")
+    }
+    func datapass(UserName: String, Password: String) {
+        UserNameLabel.text = UserName
+        PasswordLabel.text = Password
+    }
+}
+
--- PotocolAndDelegate/SceneDelegate.swift
+++ PotocolAndDelegate/SceneDelegate.swift
@@ -10,12 +10,19 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var BaseVC : UINavigationController?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        let FirstVC = ViewController()
+        BaseVC = UINavigationController(rootViewController: FirstVC)
+        BaseVC!.setNavigationBarHidden(true, animated: false)
+        window!.rootViewController = BaseVC
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
